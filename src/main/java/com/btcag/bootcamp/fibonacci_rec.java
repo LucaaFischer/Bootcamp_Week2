ec7b1b9d48bafd5f051da86603163e0ed83efd23
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-26 07:28:52 +0200

    Fibonacci bearbeitet

--- src/main/java/com/btcag/bootcamp/fibonacci_rec.java
+++ src/main/java/com/btcag/bootcamp/fibonacci_rec.java
@@ -3,19 +3,25 @@ package com.btcag.bootcamp;
 import java.util.Scanner;
 
 public class fibonacci_rec {
+    public static int fibOne = 0;
+    public static int fibTwo = 1;
+    public static int result = 0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-       // System.out.println("Wie viele Fibonacci-Zahlen willst du haben?");
-        // int n = input.nextInt();
-        System.out.println(fibonacci(20));
+        System.out.println("Wie viele Fibonacci-Zahlen willst du haben?");
+        int n = input.nextInt();
+        System.out.println(fibonacci(n));
     }
 
     public static int fibonacci(int n) {
-        if (n == 1) {
-            return 33; // random zahl zum prüfen atm
-        } else {
-            fibonacci(n-1);
-            return n;
+
+        if (n > 0 ) {
+            result = fibOne + fibTwo;
+            fibOne = fibTwo;
+            fibTwo = result;
+            fibonacci(n - 1);
         }
+            return result;
     }
 }
