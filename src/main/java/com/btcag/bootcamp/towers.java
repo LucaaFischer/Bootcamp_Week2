a7fe582ba8559c87363dd00009d58efd4ae9374e
Luca Fischer <luca.fischer@btc-ag.com>
2024-10-07 08:34:15 +0200

    Abfrage zur Ermittlung der Größten Scheibe pro Turm hinzugefügt. Doesnt work yet tho

--- src/main/java/com/btcag/bootcamp/towers.java
+++ src/main/java/com/btcag/bootcamp/towers.java
@@ -2,6 +2,8 @@ package com.btcag.bootcamp;
 
 import java.util.Scanner;
 
+
+//-------------------------------------------------------------------------Mehrfach benötigte Variablen und Arrays-----------------------------------------------------------------
 public class towers {
     public static Scanner input = new Scanner(System.in);
     public static int[] towerOne = {4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -9,95 +11,24 @@ public class towers {
     public static int[] towerThree = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     public static int temp = 0;
-    public static String result = "";
 
     public static int move = 0;
     public static int moveStart = 0;
     public static int moveDest = 0;
 
-    public static boolean solved() {
-        return (towerThree[0] == 4 && towerThree[1] == 3 && towerThree[2] == 2 && towerThree[3] == 1);
-    }
-
-    public static void printTowers() {
-        if (moveStart == 1) {
-            casesTowerOne();
-        } else if (moveStart == 2) {
-            casesTowerTwo();
-        } else if (moveStart == 3) {
-            casesTowerThree();
-        }
-        System.out.println(result);
-    }
-
-    public static String casesTowerOne() {
-        for (int i = 0; i < towerOne.length; i++) {
-            if (moveStart == 1 && moveDest == 2) {
-                if (towerOne[i] < towerTwo[i]) {
-                    temp = towerOne[i];
-                    towerOne[i] = towerTwo[i];
-                    towerTwo[i] = temp;
-                }
-            } else if (moveStart == 1 && moveDest == 3) {
-                if (towerOne[i] < towerThree[i]) {
-                    temp = towerOne[i];
-                    towerOne[i] = towerThree[i];
-                    towerThree[i] = temp;
-                }
-            }
-        }
-        result = ("Turm 1: " + java.util.Arrays.toString(towerOne) + "\n" +
-                "Turm 2: " + java.util.Arrays.toString(towerTwo) + "\n" +
-                "Turm 3: " + java.util.Arrays.toString(towerThree));
-        return result;
-    }
-
-    public static String casesTowerTwo() {
-        for (int i = 0; i < towerTwo.length; i++) {
-            if (moveStart == 2 && moveDest == 1) {
-                if (towerTwo[i] < towerOne[i]) {
-                    temp = towerTwo[i];
-                    towerTwo[i] = towerOne[i];
-                    towerOne[i] = temp;
-                }
-            } else if (moveStart == 2 && moveDest == 3) {
-                if (towerTwo[i] < towerThree[i]) {
-                    temp = towerTwo[i];
-                    towerTwo[i] = towerThree[i];
-                    towerThree[i] = temp;
-                }
-            }
-        }
-        result = ("Turm 1: " + java.util.Arrays.toString(towerOne) + "\n" +
-                "Turm 2: " + java.util.Arrays.toString(towerTwo) + "\n" +
-                "Turm 3: " + java.util.Arrays.toString(towerThree));
-        return result;
-    }
-
-    public static String casesTowerThree() {
-        for (int i = 0; i < towerThree.length; i++) {
-            if (moveStart == 3 && moveDest == 1) {
-                if (towerThree[i] < towerOne[i]) {
-                    temp = towerThree[i];
-                    towerThree[i] = towerOne[i];
-                    towerOne[i] = temp;
-                }
-            } else if (moveStart == 3 && moveDest == 2) {
-                if (towerThree[i] < towerTwo[i]) {
-                    temp = towerThree[i];
-                    towerThree[i] = towerTwo[i];
-                    towerTwo[i] = temp;
-                }
-            }
-        }
-        result = ("Turm 1: " + java.util.Arrays.toString(towerOne) + "\n" +
-                "Turm 2: " + java.util.Arrays.toString(towerTwo) + "\n" +
-                "Turm 3: " + java.util.Arrays.toString(towerThree));
-        return result;
-    }
+    //---------------------------------------------------------------------------Main---------------------------------------------------------------------------------------
     public static void main(String[] args) {
+
+        Game();
+
+        System.out.println("Gelöst :)");
+    }
+
+    //----------------------------------------------------------------------------Game-------------------------------------------------------------------------------------------
+    public static void Game() {
         while (!solved()) {
             printTowers();
+
             System.out.println("Welche Scheibe möchtest du bewegen?");
             move = input.nextInt();
 
@@ -107,6 +38,120 @@ public class towers {
             System.out.println("Auf welchen Turm?");
             moveDest = input.nextInt();
         }
-        System.out.println("Gelöst :)");
+    }
+
+
+    //----------------------------------------------------------------------Gewinnbedingung--------------------------------------------------------------------------------------
+
+    public static boolean solved() {
+        return (towerThree[0] == 4 && towerThree[1] == 3 && towerThree[2] == 2 && towerThree[3] == 1);
+    }
+
+    //----------------------------------------------------------------------Türme ausgeben--------------------------------------------------------------------------------------
+    public static String printTowers() {
+        casesTowerOne();
+        casesTowerTwo();
+        casesTowerThree();
+
+        return ("Turm 1: " + java.util.Arrays.toString(towerOne) + "\n" +
+                "Turm 2: " + java.util.Arrays.toString(towerTwo) + "\n" +
+                "Turm 3: " + java.util.Arrays.toString(towerThree));
+    }
+
+    //----------------------------------------------------------------Größte Scheibe auf Turm 1 ermitteln-----------------------------------------------------------------------------
+    public static int maxDiskT1() {
+        int maxDiscT1 = 0;
+        for (int i = 0; i < towerOne.length - 1; i++) {
+            if (towerOne[i] > towerOne[i + 1]) {
+                maxDiscT1 = towerOne[i + 1];
+            }
+        }
+        return maxDiscT1;
+    }
+
+    //----------------------------------------------------------------Größte Scheibe auf Turm 2 ermitteln-----------------------------------------------------------------------------
+    public static int maxDiskT2() {
+        int maxDiscT2 = 0;
+        for (int i = 0; i < towerTwo.length - 1; i++) {
+            if (towerTwo[i] > towerTwo[i + 1]) {
+                maxDiscT2 = towerTwo[i + 1];
+            }
+        }
+        return maxDiscT2;
+    }
+
+    //----------------------------------------------------------------Größte Scheibe auf Turm 3 ermitteln-----------------------------------------------------------------------------
+    public static int maxDiskT3() {
+        int maxDiscT3 = 0;
+        for (int i = 0; i < towerThree.length - 1; i++) {
+            if (towerThree[i] > towerThree[i + 1]) {
+                maxDiscT3 = towerThree[i + 1];
+            }
+        }
+        return maxDiscT3;
+    }
+
+    //----------------------------------------------------------------Mögliche Ausgänge für towerOne-------------------------------------------------------------------------------
+
+    public static void casesTowerOne() {
+        if (moveStart == 1) {
+            for (int i = 0; i < towerOne.length; i++) {
+                if (moveDest == 2 && maxDiskT1() < maxDiskT2()) {
+                    temp = towerOne[i];
+                    towerOne[i] = towerTwo[i];
+                    towerTwo[i] = temp;
+
+                } else if (moveDest == 3 && maxDiskT1() < maxDiskT3()) {
+                    temp = towerOne[i];
+                    towerOne[i] = towerThree[i];
+                    towerThree[i] = temp;
+
+                } else {
+                    System.out.println("Zug nicht möglich.");
+                }
+            }
+        }
+    }
+
+    //---------------------------------------------------------------Mögliche Ausgänge für towerTwo--------------------------------------------------------------------------------
+    public static void casesTowerTwo() {
+        if (moveStart == 2) {
+            for (int i = 0; i < towerTwo.length; i++) {
+                if (moveDest == 1 && maxDiskT2() < maxDiskT1()) {
+                    temp = towerTwo[i];
+                    towerTwo[i] = towerOne[i];
+                    towerOne[i] = temp;
+
+                } else if (moveDest == 3 && maxDiskT2() < maxDiskT3()) {
+                    temp = towerTwo[i];
+                    towerTwo[i] = towerThree[i];
+                    towerThree[i] = temp;
+
+                } else {
+                    System.out.println("Zug nicht möglich.");
+                }
+            }
+        }
+    }
+
+    //--------------------------------------------------------------Mögliche Ausgänge für towerThree------------------------------------------------------------------------------
+    public static void casesTowerThree() {
+        if (moveStart == 3) {
+            for (int i = 0; i < towerThree.length; i++) {
+                if (moveDest == 1 && maxDiskT3() < maxDiskT1()) {
+                    temp = towerThree[i];
+                    towerThree[i] = towerOne[i];
+                    towerOne[i] = temp;
+
+                } else if (moveDest == 2 && maxDiskT3() < maxDiskT2()) {
+                    temp = towerThree[i];
+                    towerThree[i] = towerTwo[i];
+                    towerTwo[i] = temp;
+
+                } else {
+                    System.out.println("Zug nicht möglich.");
+                }
+            }
+        }
     }
 }
